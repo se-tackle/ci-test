d997c93f8946d2cabea64c0eff6268a72aacac3a
jhlee <jun73045928@gmail.com>
2025-05-29 18:57:33 +0900

    controller 추가

---
+++ src/main/kotlin/org/swenbe/citest/Controller.kt
@@ -0,0 +1,13 @@
+package org.swenbe.citest
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class Controller {
+
+    @GetMapping("/hello")
+    fun hello(): String = "Hello World"
+}
