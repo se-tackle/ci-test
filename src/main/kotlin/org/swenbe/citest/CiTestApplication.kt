a97d3f0093ba383946e4d758b86dfe2649d11d30
jhlee <jun73045928@gmail.com>
2025-05-29 13:22:09 +0900

    initial commit

---
+++ src/main/kotlin/org/swenbe/citest/CiTestApplication.kt
@@ -0,0 +1,11 @@
+package org.swenbe.citest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CiTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<CiTestApplication>(*args)
+}
---
+++ src/test/kotlin/org/swenbe/citest/CiTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.swenbe.citest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
